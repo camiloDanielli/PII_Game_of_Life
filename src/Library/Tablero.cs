123e5972e1320d43a6629887d70ac8efbd306c65
Juanchi6 <141652338+Juanchi6@users.noreply.github.com>
2023-08-30 11:31:47 -0300

    Update Tablero.cs

--- src/Library/Tablero.cs
+++ src/Library/Tablero.cs
@@ -1,1 +1,32 @@
+public class Tablero{
 
+    public void ImprimirTablero(bool[,] b){}
+
+
+        int width 
+        int height 
+        while (true)
+        {
+            Console.Clear();
+            StringBuilder s = new StringBuilder();
+            for (int y = 0; y<height;y++)
+            {
+                for (int x = 0; x<width; x++)
+                {
+                    if(b[x,y])
+                    {
+                        s.Append("|X|");
+                    }
+                    else
+                    {
+                        s.Append("___");
+                    }
+                }
+                s.Append("\n");
+            }
+            Console.WriteLine(s.ToString());
+            b = Logica.ApplyRules(b);
+            Thread.Sleep(300);
+}
+
+}
